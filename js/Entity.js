3928203a1dbd3e32726e22a2e9e85f2e2385e441
Pontus Alexander <pontus.alexander@gmail.com>
2019-11-26 18:15:48 +0100

    Import Entity

---
+++ js/Entity.js
@@ -0,0 +1,22 @@
+import {Vec2} from "./Math.js";
+
+export function Entity() {
+    const pos = Vec2();
+    const vel = Vec2();
+    const acc = Vec2();
+
+    return {
+        acc,
+        pos,
+        vel,
+        draw(context) {
+            context.fillRect(pos.x, pos.y, 10, 10);
+        },
+        update(time) {
+            vel.x += acc.x * time;
+            vel.y += acc.y * time;
+            pos.x += vel.x * time;
+            pos.y += vel.y * time;
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 import {Timer} from "./js/Timer.js";
 import {Vec2} from "./js/Math.js";
-
-function Entity() {
-    const pos = Vec2();
-    const vel = Vec2();
-    const acc = Vec2();
-
-    return {
-        acc,
-        pos,
-        vel,
-        draw(context) {
-            context.fillRect(pos.x, pos.y, 10, 10);
-        },
-        update(time) {
-            vel.x += acc.x * time;
-            vel.y += acc.y * time;
-            pos.x += vel.x * time;
-            pos.y += vel.y * time;
-        }
-    }
-}
+import {Entity} from "./js/Entity.js";
 
 function Game() {
     const gravity = Vec2(0, 500);
